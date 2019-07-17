b5ffcd47210642bfd02dca9d7e64563b88788dce
jlanp3 <jlanp3@gmail.com>
2019-07-17 03:06:16 -0400

    test

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'novels#index'
+  resources :novels
+  testing 
 end
